6b331705906a1345c61feb4b027567637dd832b5
Jason Davis <Jasonodavis323@gmail.com>
2022-12-05 15:25:16 -0700

    sorted top comments

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 from googleapiclient.discovery import build
 import os
+import re
 from dotenv import load_dotenv
 load_dotenv()
 
 
+regex = re.compile('[,\.!?]')
 #video Id 
 videoId = 'H9AAnV59ddE'
 youtube_service_object = build('youtube', 'v3', developerKey=os.getenv('API_KEY'))
+#sorts through list of dicts based on likes 
 
 
-totalComments = []
-postingUsers = []
+
+commentStats = []
+
 #function gets comments information by iterating through items of comment threads.
 def loadText(response):
     for item in response['items']:
+
         comment = item['snippet']['topLevelComment']
         user = comment['snippet']['authorDisplayName']
         commentText = comment['snippet']['textDisplay']
+        commentLikes = comment['snippet']['likeCount']
+        commentReplies = item['snippet']['totalReplyCount']
 
-        totalComments.append(commentText)
+        commentDict = dict(user = user, text = commentText, likes = commentLikes, replies= commentReplies)
+        commentStats.append(commentDict)
+      
 
 #function returns the comment thread on a page given the page parmeter
 def getCommentThreads(page):
@@ -39,11 +48,60 @@ try:
         next_page_token = commentHelper['nextPageToken']
         loadText(commentHelper)
 except:
-    print(len(totalComments))
+   
+    print('done')
 
 
+#sorting the comment stats via likes numbers
+sortedByLikes = sorted(commentStats, key=lambda dict: dict['likes'], reverse=True)
+
+# for item in sortedByLikes:
+
+#     # print('==============\n', item['user'], "\n", item['text'], '\n', item['likes'], '\n')
 
 
+#take first 100 top comments (but make that number changeable) and save to a new list called top.
+#take the rest and save those to a new list called lesser
+top_comments = []
+lesser_comments = []
 
+def topCommentGetter(top_x):
+    for x in range(top_x):
+        top_comments.append(sortedByLikes[x])
 
+    for x in range(top_x, len(sortedByLikes)):
+        lesser_comments.append(sortedByLikes[x])
     
+    
+    
+    
+    
+   
+
+
+topCommentGetter(10)
+
+
+
+
+
+
+def top_lesser_R_getter():
+    #clean comments
+    top_cmnts_clean = []
+    for item in top_comments:
+        clean_item = item['text'].casefold()
+        clean_item = regex.sub('', clean_item)
+        top_cmnts_clean.append(clean_item.split(' '))
+    lesser_cmnts_clean = []
+    for item in lesser_comments:
+        clean_item = item['text'].casefold()
+        clean_item = regex.sub('', clean_item)
+        lesser_cmnts_clean.append(clean_item.split(' '))
+    print(top_cmnts_clean)
+               
+
+
+
+
+top_lesser_R_getter()
