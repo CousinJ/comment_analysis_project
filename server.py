a11525ba0654c91449f6b506aa807df0669a6acb
Jason Davis <Jasonodavis323@gmail.com>
2023-01-20 11:59:58 -0700

    mvp project

--- server.py
+++ server.py
@@ -2,10 +2,16 @@ from flask import Flask, redirect, url_for, render_template,request
 import ytm 
 app = Flask(__name__)
 
-comment_dict_array = []
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+    
 
 @app.route('/', methods=["GET", "POST"])
 def home():
+    comment_dict_array = []
+    sorted_topx_result = []
     if request.method == "POST":
         #getting vars from user
         youtube_video_id = request.form.get('youtube_video_id')
@@ -16,11 +22,11 @@ def home():
         print(top_comment_num)
         
         
-       
+       #initialize page with plank page argument,
         res = ytm.get_response('', youtube_video_id)
         ytm.extract_item(res, comment_dict_array)
         
-
+        #gets the next page token
         next_page = res['nextPageToken']
         res=ytm.get_response(next_page, youtube_video_id)
         ytm.extract_item(res, comment_dict_array)
@@ -38,10 +44,14 @@ def home():
             print('all pages have been processed')
 
             cda_sortedByLikes = sorted(comment_dict_array, key=lambda dict: dict['likes'], reverse=True)
+            #end result
             sorted_topx_result = ytm.get_topComments(top_comment_num, cda_sortedByLikes)
             
             
             print(sorted_topx_result)
+            #passing in the result to the template to map over in the  html with jinja2 lang.
+            return render_template('home.html', result=sorted_topx_result)
+            
         
       
         
